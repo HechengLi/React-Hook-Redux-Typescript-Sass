3878d25729057e6ac07e77264b59b66b3ecf17df
HechengLi <lihecheng93@gmail.com>
2019-08-13 11:37:01 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { AppState } from './store';
+
+import { SystemState } from './store/system/types';
+import { updateSession } from './store/system/actions';
+
+import { ChatState } from './store/chat/types';
+import { sendMessage } from './store/chat/actions';
+
+import ChatHistory from './ChatHistory';
+import ChatInterface from './ChatInterface';
+
+import { thunkSendMessage } from './thunks';
+
+interface AppProps {
+  sendMessage: typeof sendMessage
+  updateSession: typeof updateSession
+  chat: ChatState
+  system: SystemState
+  thunkSendMessage: any
+}
+
+export type UpdateMessageParam = React.SyntheticEvent<{ value: string }>
+
+const App: React.FC<AppProps> = ({ chat, system, sendMessage }) => {
+  const [message, setMessage] = useState('')
+
+  const update = (event: UpdateMessageParam) => {
+    setMessage(event.currentTarget.value);
+  };
+
+  const send = (message: string) => {
+    sendMessage({
+      user: system.username,
+      message: message,
+      timestamp: new Date().getTime()
+    });
+    setMessage("");
+  };
+
+  return (
+    <div className="App">
+      <ChatHistory messages={chat.messages} />
+      <ChatInterface
+        username={system.username}
+        message={message}
+        updateMessage={update}
+        sendMessage={send}
+      />
+    </div>
+  )
+}
+
+const mapStateToProps = (state: AppState) => ({
+  system: state.system,
+  chat: state.chat
+})
+
+export default connect(
+  mapStateToProps,
+  { sendMessage, updateSession, thunkSendMessage }
+)(App)
---
+++ src/ChatInterface.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { UpdateMessageParam } from './App'
+
+interface ChatInterfaceProps {
+  message: string
+  username: string
+  sendMessage: (message: string) => void
+  updateMessage: (event: UpdateMessageParam) => void
+}
+
+const ChatInterface: React.FC<ChatInterfaceProps> = ({
+  username,
+  message,
+  updateMessage,
+  sendMessage
+}) => {
+  function keyPress(e: React.KeyboardEvent<any>) {
+    if (e.key === 'Enter') {
+      send()
+    }
+  }
+
+  function send() {
+    sendMessage(message)
+  }
+
+  return (
+    <div className="chat-interface">
+      <h3>User: {username}</h3>
+      <input
+        value={message}
+        onChange={updateMessage}
+        onKeyPress={keyPress}
+        className="chat-input"
+        placeholder="Type a message..."
+      />
+      <button onClick={send}>Send</button>
+    </div>
+  )
+}
+
+export default ChatInterface
